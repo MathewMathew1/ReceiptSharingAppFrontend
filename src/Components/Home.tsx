a8671b9aa9b3334b94e54571dbccb848704f01d7
Mathew <mateusz.lu123@gmail.com>
2023-09-25 12:43:25 +0200

    updated home to have search query

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -4,8 +4,8 @@ import { Receipt } from "../types/Receipt";
 import ReceiptPreview from "./Receipts/ReceiptPreview";
 import { LoadingSpinner } from "./LoadingCircle";
 import { useSearchParams } from "react-router-dom";
+import {INPUT_CLASS} from "../Utils/input"
 import Button from "./Button";
-
 
 const SELECTION_OPTIONS = [
   {name: "Newest", queryParam: "newest"},
@@ -20,6 +20,8 @@ export default function Home() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [searchType, setSearchType] = useState(searchParams.get("sort") || SELECTION_OPTIONS[0].queryParam)
   const [fetchedReceipts, setFetchedReceipts] = useState(false)
+  const [disableSearchButton, setDisableSearchButton] = useState(false)
+  const [search, setSearch] = useState(searchParams.get("search")? searchParams.get("search")!: "")
   
 
   const [isThereNextPage, setIsThereNextPage] = useState(false)
@@ -29,11 +31,16 @@ export default function Home() {
   useEffect(() => {
     const getReceipts = async () => {
       try{
+        setDisableSearchButton(true)
 
         const { signal } = controller
         setFetchedReceipts(false)
-        console.log(searchParams.get("page"))
-        const response = await fetch(RECEIPTS_ROUTE+`?sortType=${searchParams.get("sort")}&limit=${LIMIT_PER_PAGE}&skip=${0}`,{
+        const page =searchParams.get("page")? parseInt(searchParams.get("page")!): 0
+        const skip = page * LIMIT_PER_PAGE
+
+        const searchParam = searchParams.get("search")? searchParams.get("search"): ""
+
+        const response = await fetch(RECEIPTS_ROUTE+`?sortType=${searchParams.get("sort")}&limit=${LIMIT_PER_PAGE}&skip=${skip}&searchQuery=${searchParam}`,{
           method: "GET",
           signal
         })
@@ -48,12 +55,13 @@ export default function Home() {
       } 
       finally{
         setFetchedReceipts(true)
+        setDisableSearchButton(false)
       }
     }
     
     document.title = "Receptao";
     getReceipts()
-  }, [searchParams.get("page"), searchParams.get("sort")]);
+  }, [searchParams.get("page"), searchParams.get("sort"), searchParams.get("search")]);
 
   useEffect(() => {
 
@@ -70,6 +78,10 @@ export default function Home() {
     setSearchParams({["page"]: (page+changeInPage).toString()})
   }
 
+  const setParamsBeforeSearch = () => {
+    setSearchParams({["search"]: search})
+  }
+
   return (
     <div className="mt-5 flex flex-col">
       <div className="mb-5">
@@ -83,28 +95,40 @@ export default function Home() {
                 <option value={option.queryParam} key={`option ${index}`}>{option.name}</option>
             ))}
           </select>
+          <input
+            aria-placeholder={"search"}
+            type="text"
+            className={`${INPUT_CLASS} border ml-[2rem] px-4 py-1 focus:outline-none focus:border-blue-500`}
+            placeholder="Search..."
+            value={search}
+            onChange={(e)=>setSearch(e.target.value)}
+          />
+          <Button 
+            className="ml-4"
+            disabled={disableSearchButton} 
+            color="green" 
+            onClick={()=>setParamsBeforeSearch()}>
+            Search
+          </Button>
         </div>
         {fetchedReceipts?
         <div>
-          <div className="flex justify-center gap-5 mt-4 align-center flex-wrap">
-              {receipts.map((receipt, index) => (
-                <ReceiptPreview receipt={receipt} key={`receipt ${index}`}/>
-              ))
+          {receipts.length > 0?
+            <div className="flex justify-center gap-5 mt-4 align-center flex-wrap">
+                {receipts.map((receipt, index) => (
+                  <ReceiptPreview receipt={receipt} key={`receipt ${index}`}/>
+                ))
 
-              }
-          </div>
-          <div className="mt-5">
-            {page!==1?
-              <Button color="blue" onClick={()=>nextPage(-1)}>Previous</Button>
-            :
-              null
-            }
-            {isThereNextPage?
-              <Button color="blue" onClick={()=>nextPage(1)}>Next</Button>
-            :
-              null
-            }
-          
+                }
+            </div>
+          :
+            <div className="my-4 text-[1.2rem]">
+               No Receipts have been found for this query   
+            </div>
+          }
+          <div className="mt-5 flex gap-1 justify-center">
+              <Button color="blue" disabled={page===1} onClick={()=>nextPage(-1)}>Previous</Button>
+              <Button color="blue" disabled={!isThereNextPage} onClick={()=>nextPage(1)}>Next</Button>
           </div>
         </div>
         :
